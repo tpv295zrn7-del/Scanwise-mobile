86d6f33b414fff841fdace16affa07009fa3b68c
tpv295zrn7-del <tpv295zrn7-del@users.noreply.github.com>
2026-07-26 19:13:15 +0000

    fix: limit Metro file watching to prevent EMFILE on macOS

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Prevent EMFILE crash on macOS by limiting file watching
+// to the project directory instead of crawling node_modules
+config.watchFolders = [__dirname];
+config.resetCache = true;
+
 module.exports = config;
